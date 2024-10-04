4da1e0a34829ff445dd8aa9c5a07c3258d0685d2
1378544 <51883638+1378544@users.noreply.github.com>
2024-10-04 16:29:36 +0200

    Add files via upload

---
+++ FOP/FOP/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace FOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.execute = new System.Windows.Forms.Button();
+            this.inject = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.fileSystemWatcher2 = new System.IO.FileSystemWatcher();
+            this.Editor = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.robloxopen = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.X = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Button = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Editor)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // execute
+            // 
+            this.execute.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.execute.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.execute.ForeColor = System.Drawing.SystemColors.Control;
+            this.execute.Location = new System.Drawing.Point(12, 424);
+            this.execute.Name = "execute";
+            this.execute.Size = new System.Drawing.Size(81, 36);
+            this.execute.TabIndex = 0;
+            this.execute.Text = "Exectue";
+            this.execute.UseVisualStyleBackColor = false;
+            this.execute.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inject
+            // 
+            this.inject.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.inject.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.inject.ForeColor = System.Drawing.SystemColors.Control;
+            this.inject.Location = new System.Drawing.Point(811, 422);
+            this.inject.Name = "inject";
+            this.inject.Size = new System.Drawing.Size(93, 38);
+            this.inject.TabIndex = 1;
+            this.inject.Text = "Inject";
+            this.inject.UseVisualStyleBackColor = false;
+            this.inject.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // fileSystemWatcher2
+            // 
+            this.fileSystemWatcher2.EnableRaisingEvents = true;
+            this.fileSystemWatcher2.SynchronizingObject = this;
+            // 
+            // Editor
+            // 
+            this.Editor.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.Editor.AutoIndentCharsPatterns = "^\\s*[\\w\\.]+(\\s\\w+)?\\s*(?<range>=)\\s*(?<range>[^;=]+);\r\n^\\s*(case|default)\\s*[^:]*" +
+    "(?<range>:)\\s*(?<range>[^;]+);";
+            this.Editor.AutoScrollMinSize = new System.Drawing.Size(411, 18);
+            this.Editor.BackBrush = null;
+            this.Editor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.Editor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Editor.CharHeight = 18;
+            this.Editor.CharWidth = 10;
+            this.Editor.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Editor.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.Editor.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.Editor.IndentBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.Editor.IsReplaceMode = false;
+            this.Editor.LineNumberColor = System.Drawing.Color.MintCream;
+            this.Editor.Location = new System.Drawing.Point(7, 84);
+            this.Editor.Name = "Editor";
+            this.Editor.Paddings = new System.Windows.Forms.Padding(0);
+            this.Editor.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.Editor.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("Editor.ServiceColors")));
+            this.Editor.ServiceLinesColor = System.Drawing.Color.Empty;
+            this.Editor.Size = new System.Drawing.Size(761, 335);
+            this.Editor.TabIndex = 2;
+            this.Editor.Text = "--Made By Koashi (Discord:koashi_toru)";
+            this.Editor.Zoom = 100;
+            this.Editor.Load += new System.EventHandler(this.Editor_Load);
+            // 
+            // robloxopen
+            // 
+            this.robloxopen.AutoSize = true;
+            this.robloxopen.BackColor = System.Drawing.Color.Transparent;
+            this.robloxopen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.robloxopen.Font = new System.Drawing.Font("Impact", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.robloxopen.ForeColor = System.Drawing.Color.Black;
+            this.robloxopen.Location = new System.Drawing.Point(77, 47);
+            this.robloxopen.Name = "robloxopen";
+            this.robloxopen.Size = new System.Drawing.Size(78, 17);
+            this.robloxopen.TabIndex = 4;
+            this.robloxopen.Text = "Roblox Open:";
+            this.robloxopen.Click += new System.EventHandler(this.robloxopen_Click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.BackColor = System.Drawing.Color.Black;
+            this.status.Font = new System.Drawing.Font("Impact", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.status.ForeColor = System.Drawing.Color.White;
+            this.status.Location = new System.Drawing.Point(371, 47);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(46, 17);
+            this.status.TabIndex = 5;
+            this.status.Text = "Status:";
+            this.status.Click += new System.EventHandler(this.status_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.X);
+            this.panel1.Location = new System.Drawing.Point(0, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(914, 42);
+            this.panel1.TabIndex = 6;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Impact", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(843, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(31, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // X
+            // 
+            this.X.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.X.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.X.Font = new System.Drawing.Font("Impact", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.X.ForeColor = System.Drawing.Color.White;
+            this.X.Location = new System.Drawing.Point(880, 3);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(31, 31);
+            this.X.TabIndex = 0;
+            this.X.Text = "X";
+            this.X.UseVisualStyleBackColor = false;
+            this.X.Click += new System.EventHandler(this.X_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.ForeColor = System.Drawing.Color.White;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(774, 94);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(130, 304);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Button
+            // 
+            this.Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button.ForeColor = System.Drawing.SystemColors.Control;
+            this.Button.Location = new System.Drawing.Point(784, 333);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(111, 32);
+            this.Button.TabIndex = 8;
+            this.Button.Text = "Refresh";
+            this.Button.UseVisualStyleBackColor = false;
+            this.Button.Click += new System.EventHandler(this.Button_Click);
+            this.Button.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button_MouseDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::VoidX.Properties.Resources.DALL_E_2024_10_04_08_53_removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(29, 27);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.label1.Location = new System.Drawing.Point(361, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Current Version: 1.0 (Beta)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(35, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "VoidX";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(916, 472);
+            this.Controls.Add(this.Button);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.robloxopen);
+            this.Controls.Add(this.Editor);
+            this.Controls.Add(this.inject);
+            this.Controls.Add(this.execute);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "FOP";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Editor)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button execute;
+        private System.Windows.Forms.Button inject;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.IO.FileSystemWatcher fileSystemWatcher2;
+        private FastColoredTextBoxNS.FastColoredTextBox Editor;
+        private System.Windows.Forms.Label robloxopen;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button X;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FOP/FOP/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FOP
+{
+    public partial class Form1 : Form
+    {
+        Timer time = new Timer();        // Existing timer for other purposes
+        Timer fadeOutTimer = new Timer();  // Timer for fade-out animation
+        public Point mouseLocation;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set up the regular timer
+            time.Tick += timertick;
+            time.Start();
+
+            // Set up the fade-out timer
+            fadeOutTimer.Interval = 10;  // Interval in milliseconds for smooth fade
+            fadeOutTimer.Tick += fadeOutTimer_Tick;  // Link fade timer to event handler
+        }
+
+        // Existing method to check Roblox status and update UI
+        private void timertick(object sender, EventArgs e)
+        {
+            if (ForlornApi.Api.IsRobloxOpen())
+            {
+                robloxopen.Text = " ";
+                robloxopen.ForeColor = Color.LightGreen;
+            }
+            else
+            {
+                robloxopen.Text = " ";
+                robloxopen.ForeColor = Color.Red;
+            }
+
+            if (ForlornApi.Api.IsInjected())
+            {
+                status.Text = "Status: Injected!";
+                status.ForeColor = Color.LightGreen;
+            }
+            else
+            {
+                status.Text = "Status: Not Injected!";
+                status.ForeColor = Color.Red;
+            }
+        }
+
+        // Fade-out logic to reduce opacity step-by-step
+        private void fadeOutTimer_Tick(object sender, EventArgs e)
+        {
+            if (this.Opacity > 0)  // Reduce opacity gradually
+            {
+                this.Opacity -= 0.05;  // Decrease opacity by 5% at each tick
+            }
+            else
+            {
+                fadeOutTimer.Stop();  // Stop the timer when opacity reaches 0
+                this.Close();  // Close the form after fade-out
+            }
+        }
+
+        // X button click event with fade-out effect
+        private void X_Click(object sender, EventArgs e)
+        {
+            fadeOutTimer.Start();  // Start the fade-out animation
+        }
+
+        // Other methods (existing functionality)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ForlornApi.Api.Inject();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ForlornApi.Api.ExecuteScript(Editor.Text);
+        }
+
+        private void status_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Editor.Text = File.ReadAllText($"./Scripts/{listBox1.SelectedItem}");
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            functions.PopulateListBox(listBox1, "./Scripts", "*.lua");
+            functions.PopulateListBox(listBox1, "./Scripts", "*.txt");
+        }
+
+        private void Button_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseLocation = new Point(-e.X, -e.Y);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mousePose = Control.MousePosition;
+                mousePose.Offset(mouseLocation.X, mouseLocation.Y);
+                Location = mousePose;
+            }
+        }
+
+        private void Editor_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void robloxopen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    // Helper class for populating ListBox
+    class functions
+    {
+        public static void PopulateListBox(System.Windows.Forms.ListBox lsb, string Folder, string FileType)
+        {
+            DirectoryInfo dinfo = new DirectoryInfo(Folder);
+            FileInfo[] Files = dinfo.GetFiles(FileType);
+            foreach (FileInfo file in Files)
+            {
+                lsb.Items.Add(file.Name);
+            }
+        }
+    }
+}
